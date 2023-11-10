aa7808d81eecf7e706e7912086639d73fe8a8f0a
namonter <nathamonte258@gmail.com>
2023-11-09 17:42:37 -0800

    Adding initial Nova files

---
+++ Nova/src/main/java/group9/nova/Nova.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package group9.nova;
+
+/**
+ *
+ * @author Nathan
+ */
+public class Nova {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
