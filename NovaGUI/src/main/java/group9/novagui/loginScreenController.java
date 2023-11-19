fa93415d2e73a059d3d5f1b485fb2afa6b4aa7ed
namonter <nathamonte258@gmail.com>
2023-11-18 20:42:42 -0800

    Account Creation Alert
    Implemented a GUI alert for creating your account.

--- NovaGUI/src/main/java/group9/novagui/loginScreenController.java
+++ NovaGUI/src/main/java/group9/novagui/loginScreenController.java
@@ -1,12 +1,15 @@
 package group9.novagui;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
-public class loginScreenController { //implements Initializable
+public class loginScreenController {
     
     int loginPressCounter = 0;
     @FXML
@@ -25,5 +28,23 @@ public class loginScreenController { //implements Initializable
             labelSignInError.setText("The username or password you entered is wrong. (" + loginPressCounter + ")");
         }
 //        Account newAcc = new Account("user","pw"); 
+    }
+    @FXML
+    private void createAccountClick() throws IOException{
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Create Account Alert");
+        alert.setContentText("Are you sure you want to create an account?");
+        alert.showAndWait().ifPresent(response ->{
+            if(response == ButtonType.OK){
+                try{
+                    App.setRoot("MainScreen");
+                }
+                catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        
+        Account newAccount = new Account("user","pw");
     }
 }
