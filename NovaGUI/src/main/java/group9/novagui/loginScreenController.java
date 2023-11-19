4c5a5aa319d390ee0c6ec5a4532acc9ab935dbe0
namonter <nathamonte258@gmail.com>
2023-11-18 19:13:32 -0800

    login check for username
    Currently hardcoded so that username must be "user" to login. Counts number of wrong logins. *Working version*

--- NovaGUI/src/main/java/group9/novagui/loginScreenController.java
+++ NovaGUI/src/main/java/group9/novagui/loginScreenController.java
@@ -2,12 +2,28 @@ package group9.novagui;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
 
-public class loginScreenController {
-
+public class loginScreenController { //implements Initializable
+    
+    int loginPressCounter = 0;
+    @FXML
+    private TextField tfUsername;
+    @FXML
+    private Label labelSignInError;
     @FXML
     private void logInClick() throws IOException {  //Add code for login check here
-        App.setRoot("MainScreen");  //Switches to main screen
-        Account newAcc = new Account("user","pw");
+        loginPressCounter++;
+        if(tfUsername.getText().equals("user")){
+            App.setRoot("MainScreen");  //Switches to main screen
+            loginPressCounter = 0;
+            labelSignInError.setText("");
+        }
+        else{
+            labelSignInError.setText("The username or password you entered is wrong. (" + loginPressCounter + ")");
+        }
+//        Account newAcc = new Account("user","pw"); 
     }
 }
