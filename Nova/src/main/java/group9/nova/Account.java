8195a6e27fc13b612f5f9d7163ac8d5ee810a9e4
Rania <144974956+mardiniRania@users.noreply.github.com>
2023-11-27 10:59:14 -0800

    Update to Account, AccountTest
    Take #2 without merge issues, hopefully.
    
    - Updated methods for validating username and password.
    - Added a method to read from the file.
    - Integrated a method to save account data.
    - Updated the method to change passwords.
    - Conducted testing for methods (excluding Account Deletion).

--- Nova/src/main/java/group9/nova/Account.java
+++ Nova/src/main/java/group9/nova/Account.java
@@ -1,9 +1,17 @@
+package group9.nova;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 
 public class Account {
+    private static final String filePath = "accounts.txt"; // constant
     private String username;
     private String password;
 
@@ -13,69 +21,89 @@ public class Account {
     }
 
     public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username; // this is a placeholder, depends on where validation happens
-        saveAccountData();
+        return readFromFile("username");
     }
 
     public String getPassword() {
-        return password;
+        return readFromFile("password");
+    }
+
+    public void setUsername(String username) {
+        String existingUsername = readFromFile("username");
+
+        if (existingUsername != null && existingUsername.equals(username)) {
+            System.out.println("Duplicate username. Please choose another.");
+        } else {
+            this.username = username;
+            saveAccountData();
+        }
     }
 
     public void setPassword(String password) {
-        this.password = password; // this is a placeholder
-        saveAccountData();
+        if (password.length() < 8 && password.length() > 15) {
+            System.out.println("Invalid password. Must be between 8-15 characters.");
+        } else {
+            this.password = password;
+            saveAccountData();
+        }
+        
     }
 
-    // createAccount can also do validation for username/pw creation
-    public static Account createAccount(String username, String password) {
-        // validating input
+    public String readFromFile(String fieldName) {
+        try (Scanner scanner = new Scanner(new File(filePath))) {
+            Pattern pattern = Pattern.compile(fieldName + ": '\\s*(.*?)\\s*'");
 
-        // checking for duplicate username
-        
-        // validating pw ex
-        if (password.length() <= 8 || password.length() >= 15) {
-            // maybe better to throw illegal arg if it's built in for pw validation
-            System.out.println("Invalid password. Must be between 8 - 15 characters.");
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                Matcher match = pattern.matcher(line);
+
+                if (match.find()) {
+                    return match.group(1);
+                } else {
+                    System.out.println("Field not found: " + fieldName);
+                    return null;
+                }
+            }
+        return username; // ?? if I remove this, it gives me an error. i need to figure it out bro
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
+    }
 
+    public static Account createAccount(String username, String password) {
         Account newAccount = new Account(username, password);
-
         newAccount.saveAccountData();
-
         return newAccount;
     }
 
     // saving to the .txt file
     private void saveAccountData() {
-        try {
-            FileWriter user_file = new FileWriter("user_info.txt");
-            BufferedWriter writer = new BufferedWriter(user_file);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter (filePath, true))) {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
             
-            String formattedData = String.format("user1 username: '%s', password: '%s'", username, password);
-            
+            String formattedData = String.format("username: '%s', password: '%s'\n", username, password);
             writer.write(formattedData);
-            writer.newLine();
-
-            writer.close();
         } catch (IOException except) {
             except.printStackTrace();
         }
-        
     }
-
-    // do we need a method to read data from the .txt
 
     public boolean login(String enteredUsername, String enteredPassword) {
         return username.equals(enteredUsername) && password.equals(enteredPassword);
     }
 
-    public void changePassword(String newPassword) {
-        this.password = newPassword;
-        saveAccountData();
+    public void changePassword(String oldPassword, String newPassword) {
+        if (password.equals(oldPassword)) {
+            this.password = newPassword;
+            saveAccountData();
+            System.out.println("Password changed successfully.");
+        } else {
+            System.out.println("Wrong Password. Unable to change password.");
+        }
     }
 
     public void deleteAccount() {
