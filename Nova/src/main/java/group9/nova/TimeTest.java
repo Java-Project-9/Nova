353679b3c68136377f094ffea4cbd072f315ad99
jjpastores <pastoresj02@gmail.com>
2023-11-28 13:35:10 -0800

    Added words on time test.

--- Nova/src/main/java/group9/nova/TimeTest.java
+++ Nova/src/main/java/group9/nova/TimeTest.java
@@ -14,7 +14,7 @@ public class TimeTest {
     public static void main(String[] args) {
         Time t1 = new Time(23, 18, 23);
         LocalTime lt = LocalTime.now();
-        System.out.println(lt); 
+        System.out.printf("Local time is: %s\n", lt.now()); 
         System.out.printf("Time: %s", t1.toString());
         
         
