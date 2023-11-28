81ee22c88fb75d5ea299da1732edf480bf06c75f
jjpastores <pastoresj02@gmail.com>
2023-11-28 13:28:50 -0800

    New update on time test.
    Cannot be used in account test yet

--- Nova/src/main/java/group9/nova/TimeTest.java
+++ Nova/src/main/java/group9/nova/TimeTest.java
@@ -3,16 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package group9.nova;
+import java.time.*;
 
 /**
  *
  * @author xxcoo
  */
-public class TimeTest {
 
+public class TimeTest {
     public static void main(String[] args) {
         Time t1 = new Time(23, 18, 23);
-        
+        LocalTime lt = LocalTime.now();
+        System.out.println(lt); 
         System.out.printf("Time: %s", t1.toString());
+        
+        
     }
 }
